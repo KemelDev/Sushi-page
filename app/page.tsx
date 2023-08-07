7ddfa3e1bea15d413867e307d8ba8f408cc63390
Kevin Bolanos <kevin.bolanos1193@gmail.com>
2023-08-06 21:21:11 -0600

    Created components and change on app/page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
                 <h1>Hello this is the Sushi-man web page</h1>
+                <h2> Un cambio para comparar en github</h2>
             </div>
         </main>
     )
